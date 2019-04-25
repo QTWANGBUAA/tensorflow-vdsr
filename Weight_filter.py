2379728fec7d85e3fe365c57ea51c36ef5b5a235
QTWANGBUAA <m18829236034@163.com>
2019-04-25 19:24:50 +0800

    this is sigma refine

---
+++ Weight_filter.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from PIL import Image #或直接import Image
+import numpy as np
+image_raw_data_jpg = tf.gfile.FastGFile('/media/king/AC88794B887914D4/PycharmProjects/UPSAMPLE/result/res3.jpg', 'rb').read()
+
+
+def filter(src_img):
+    filter = tf.reshape(tf.constant([1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9, 1.0/9]), [3, 3, 1, 1])
+    mean = tf.nn.depthwise_conv2d(src_img, filter, strides=[1, 1, 1, 1],  rate=[1, 1], padding='SAME')
+    mean_square = tf.square(mean)
+    src_img_sauqre = tf.square(src_img)
+    square_mean = tf.nn.depthwise_conv2d(src_img_sauqre, filter, strides=[1, 1, 1, 1], rate=[1, 1], padding='SAME')
+    var = tf.abs(square_mean - mean_square)
+    var = tf.reshape(var, [3, 616, 816])
+    return var
+
+
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#
+#     img_data_jpg = tf.image.decode_jpeg(image_raw_data_jpg) #图像解码
+#     img_data_jpg = tf.image.convert_image_dtype(img_data_jpg, dtype=tf.float32) #改变图像数据的类型
+#     img_data_jpg = tf.reshape(img_data_jpg, [1, 616, 816, 1])
+#     var_res = filter(img_data_jpg)
+#     var_ = var_res.eval()
+#     var_ = var_*255
+#     var_res = var_.astype(np.uint8)
+#     res = Image.fromarray(var_res)
+#     res.save("./res.jpg")
+
